5c7f1a425cd839470f2cd12bf357a943a71fdbb8
Kevin Abreu <kevinrabreu@hotmail.com>
2019-06-27 16:20:02 -0400

    Starto

---
+++ server.rb
@@ -0,0 +1,20 @@
+require "sinatra"
+require "httparty"
+
+get "/" do
+  erb :home
+end
+
+get "/catalog" do
+  erb :catalog
+end
+
+get "/layout" do
+  erb :layout
+end
+
+get "/results" do
+  erb :results
+end
+
+# remember to bundle install when I'm using my own computer
